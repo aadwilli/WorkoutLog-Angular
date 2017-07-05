b9e2494089a3f04747053f2d03610abad370a87e
aadwilli <aadwilli15@gmail.com>
2017-07-05 16:39:16 -0400

    added nav bar

--- content/bundle.js
+++ content/bundle.js
@@ -86,7 +86,37 @@
 
 
 
+(function() {
+	angular.module('workoutlog')
+	.directive('userlinks',
+		function() {
+			UserLinksController.$inject = [ '$state', 'CurrentUser', 'SessionToken' ];
+			function UserLinksController($state, CurrentUser, SessionToken) {
+				var vm = this;
+				vm.user = function() {
+					return CurrentUser.get();
+				};
 
+				vm.signedIn = function() {
+					return !!(vm.user().id);
+				};
+
+				vm.logout = function() {
+					CurrentUser.clear();
+					SessionToken.clear();
+					$state.go('signin');
+				};
+			}
+
+			return {
+				scope: {},
+				controller: UserLinksController,
+				controllerAs: 'ctrl',
+				bindToController: true,
+				templateUrl: '/components/auth/userlinks.html'
+			};
+		});
+})();
 (function() {
 	angular.module('workoutlog.define', [
 			'ui.router'
