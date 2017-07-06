ccf14c3584b3f1d2a505cc7d8f98022bf06cb86c
aadwilli <aadwilli15@gmail.com>
2017-07-06 09:04:08 -0400

    deployment

--- content/bundle.js
+++ content/bundle.js
@@ -1,12 +1,21 @@
 (function(){
 	var app = angular.module('workoutlog', [
 			'ui.router',
-			'workoutlog.auth.signup',
-			'workoutlog.auth.signin',
 			'workoutlog.define',
 			'workoutlog.logs',
-			'workoutlog.history'
+			'workoutlog.history',
+			'workoutlog.feed',
+			'workoutlog.auth.signup',
+			'workoutlog.auth.signin'
 		]);
+	.factory('socket', function(socketFactory){
+		var myIoSocket = io.connect('http://localhost:3000');
+
+		var socket = socketFactory({
+			ioSocket: myIoSocket
+		});
+		return socket;
+	});
 	
 	function config($urlRouterProvider) {
 		$urlRouterProvider.otherwise('/signin');
@@ -14,7 +23,10 @@
 
 	config.$inject = [ '$urlRouterProvider' ];
 	app.config(config);
-	app.constant('API_BASE', '//localhost:3000/api/');
+
+	var API_BASE = location.hostname === "localhost" ?
+		"//localhost:3000/api/" : "//gutbuster-api.herokuapp.com/api/";
+	app.constant('API_BASE', API_BASE);
 })();
 (function(){
 	angular
